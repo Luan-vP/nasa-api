ece29bc9986f2769bd8bfb59b69f04b18668a577
Luan van Pletsen <luanvanpletsen@gmail.com>
2022-02-06 21:59:08 +0000

    Admin views

--- neuralAPI/polls/admin.py
+++ neuralAPI/polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header = "Neural API Admin" # Header displayed at the top of the admin page
+admin.site.site_title = "Neural API Admin Portal" # Title displayed in the tab of the admin page
+admin.site.index_title = "Welcome to Neural API Admin Portal" # Title displayed in the tab for the main page
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets =  [(None, {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
